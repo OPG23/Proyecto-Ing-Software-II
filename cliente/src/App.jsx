b32f584a4453702bfc02c169edf4f1ff5c6836fb
OPG23 <141506589+OPG23@users.noreply.github.com>
2025-05-27 23:08:39 -0500

    Add files via upload
    Conecto mi parte con la de Nicolas, Agrego botón para Navegar entre paginas

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -3,6 +3,8 @@ import { ProveedorAutenticacion } from "./context/ContextoAutenticacion";
 import PaginaInicioSesion from "./pages/PaginaInicioSesion";
 import PaginaRegistro from "./pages/PaginaRegistro";
 import PaginaPerfil from "./pages/paginaPerfil";
+//4.Agregre esta linea para que haya pagina vivisble de recuperacion
+import PaginaRestablecerContrasena from "./pages/PaginaRestablecerContrasena";
 
 import RutasProtegidas from "./RutasProtegidas";
 import ProteccionRutaRegistro from "./ProteccionRutaRegistro";
@@ -11,6 +13,8 @@ import ProteccionRutaPerfil from "./ProteccionRutaPerfil";
 import Navegacion from "./layouts/Navegacion";
 import fondo from '/imagenFondoInicioSesion.png'
 
+//Agregamos esta proteccion para la ruta de restablecer contraseña
+//import ProteccionRutaRestablecerContrasena from "./ProteccionRutaRestablecerContrasena";
 function App() {
   return (
     <div className="min-h-screen bg-cover bg-center flex items-center justify-center" 
@@ -19,9 +23,14 @@ function App() {
         <BrowserRouter>
           <Navegacion />
           <Routes>
+            {/* se agrega aca, para que solo se puede acceder si no se ha inicado sesion*/}
             <Route element={<ProteccionRutaIniciarSesion />}>
               <Route path="/" element={<PaginaInicioSesion />} />
+              {/*Agrego mi ruta de cambio de contrasena*/}
+              <Route path="/restablecer-contrasena" element={<PaginaRestablecerContrasena />} />
             </Route>
+
+            {/* Esta es la cosa para agregar paginas protegiendo y forzando estar logeado para ver */}
             <Route element={<RutasProtegidas />}>
               <Route element={<ProteccionRutaRegistro />}>
                 <Route path="/registro" element={<PaginaRegistro />} />
